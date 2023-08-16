1c7bf26d5eec7a918c38a6b9bc54c513b9f9518c
unknown <juan_diego_toro@hotmail.com>
2023-08-15 19:37:08 -0500

    version1 depilation.4 .env updated with correct credentials and aws-config.js file created

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const routerApi = require('./routes')
+const routerApi = require('./routes');
+const { send } = require("micro");
+const AWS = require("../aws-config.js");
 
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandler')
+// const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandler')
 
 const app = express();
 const port = 8000;
@@ -18,11 +20,11 @@ routerApi(app);
 
 //Error midlewares must to be used after routing implementation
 //Order is a condition to deploy
-app.use(logErrors);
-app.use(ormErrorHandler);
-app.use(boomErrorHandler);
-app.use(errorHandler);
+// app.use(logErrors);
+// app.use(ormErrorHandler);
+// app.use(boomErrorHandler);
+// app.use(errorHandler);
 
-app.listen(port, () =>{
-  //console.log("sisas parce! mi port" + port);
-})
+// app.listen(port, () =>{
+//   //console.log("sisas parce! mi port" + port);
+// })
