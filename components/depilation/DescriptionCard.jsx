d673d83848f4798d3b7fb751a939f07bec01b564
unknown <juan_diego_toro@hotmail.com>
2023-08-18 14:04:00 -0500

    version1 depilation.6 S3 working with depilation successfully

--- components/depilation/DescriptionCard.jsx
+++ components/depilation/DescriptionCard.jsx
@@ -9,100 +9,124 @@ const bodyZone = [
     id: 1,
     name: "cuerpo completo",
     price: "$98.000 COP",
-    image: "/img/depilation/fullbody.png",
+    image: "",
   },
   {
     id: 2,
     name: "rostro completo",
     price: "$45.000 COP",
-    image: "/img/depilation/rostro-completo.png",
+    image: "",
   },
   {
     id: 2,
     name: "bigote",
     price: "$10.000 COP",
-    image: "/img/depilation/bigote.png",
+    image: "",
   },
   {
     id: 2,
     name: "cejas",
     price: "$15.000 COP",
-    image: "/img/depilation/cejas.png",
+    image: "",
   },
   {
     id: 2,
-    name: "mentón",
+    name: "menton",
     price: "$10.000 COP",
-    image: "/img/depilation/menton.jpg",
+    image: "",
   },
   {
     id: 2,
     name: "axilas",
     price: "$15.000 COP",
-    image: "/img/depilation/axilas.jpeg",
+    image: "",
   },
   {
     id: 2,
     name: "brazos",
     price: "$25.000 COP",
-    image: "/img/depilation/brazos.jpeg",
+    image: "",
   },
   {
     id: 3,
-    name: "bikini completo (Brasileño)",
+    name: "bikini completo",
     price: "$45.000 COP",
-    image: "/img/depilation/bikini-completo.jpeg",
+    image: "",
   },
   {
     id: 3,
     name: "bikini parcial",
     price: "$30.000 COP",
-    image: "/img/depilation/bikini-parcial.png",
+    image: "",
   },
   {
     id: 3,
-    name: "glúteos",
+    name: "gluteos",
     price: "$30.000 COP",
-    image: "/img/depilation/gluteos.jpeg",
+    image: "",
   },
   {
     id: 4,
-    name: "Pierna completa",
+    name: "piernas completas",
     price: "$45.000 COP",
-    image: "/img/depilation/piernas-completas.jpg",
+    image: "",
   },
   {
     id: 4,
-    name: "Media pierna",
+    name: "media pierna",
     price: "$30.000 COP",
-    image: "/img/depilation/media-pierna.jpeg",
+    image: "",
   },
   ]
 
 const DescriptionCard = ({cardSelected}) => {
   const [services, setServices] = useState([]);
   const [currentImage, setCurrentImage] = useState(0);
-
-  const [imageUrls, setImageUrls] = useState([]);
+  const [updateBodyZone, setUpdateBodyZone] = useState([]);
 
   useEffect(() => {
     async function fetchImageUrls(){
       try {
-          const imageUrl = s3.getSignedUrl('getObject', {
-            Bucket: 'depilation',
-            Key: 'media-pierna.jpeg',
-            Expires: 60 * 5,
+        const bucketName = 'depilation';
+        const objects = await s3.listObjectsV2({ Bucket: bucketName}).promise();
+        function matchingObject(keyValue, obj){
+          let imageName = ''
+          const matchingImage = objects.Contents.find((object) => {
+            if (object.Key.includes(keyValue) && object.Key.endsWith(".png")){
+              imageName = keyValue.concat(".png");
+              return true;
+            }else if(object.Key.includes(keyValue) && object.Key.endsWith(".jpg")){
+              imageName = keyValue.concat(".jpg");
+              return true;
+            }else if(object.Key.includes(keyValue) && object.Key.endsWith(".jpeg")){
+              imageName = keyValue.concat(".jpeg");
+              return true;
+            };
           });
-
-
-
-        setImageUrls(imageUrl);
+          if (matchingImage){
+            const url = s3.getSignedUrl('getObject', {
+              Bucket: bucketName,
+              Key: matchingImage.Key,
+              Expires: 60 * 5,
+            });
+            return { ...obj, image: url };
+          };
+          return obj;
+        }
+        const bodyZoneFilter = bodyZone.map((obj) => {
+          if (obj.id === 1){
+            return matchingObject('fullbody.png', obj);
+          }else if (obj.id === 2 || obj.id === 3 || obj.id === 4){
+            const nameInBucket= obj.name.split(' ').join('-');
+            return matchingObject(nameInBucket, obj);
+          };
+        });
+        setUpdateBodyZone(bodyZoneFilter);
       } catch (error) {
         console.error('Error fetching image URLs:', error);
       }
     }
-
-    fetchImageUrls();
+  fetchImageUrls();
   }, []);
 
   const handleNextImage = () => {
@@ -113,78 +137,62 @@ const DescriptionCard = ({cardSelected}) => {
     setCurrentImage(currentImage === 0 ? services.length - 1 : currentImage - 1);
   };
 
-  // useEffect(() => {
-  //   if (cardSelected != 0){
-  //     let serviceSelected = bodyZone.filter((part) => part.id === cardSelected);
-  //     setServices(serviceSelected);
-  //     setCurrentImage(0);
-  //   }
-  // }, [cardSelected]);
+  useEffect(() => {
+    if (cardSelected != 0){
+      let serviceSelected = updateBodyZone.filter((part) => part.id === cardSelected);
+      setServices(serviceSelected);
+      setCurrentImage(0);
+    }
+  }, [cardSelected, updateBodyZone]);
 
-  return <section className="container">
-    <h2>Image Gallery</h2>
-    <div>
-        <img src={imageUrls} alt="" style={{ maxWidth: '300px', margin: '10px' }} />
+  return (<section className="container">
+  {services.length === 1 ? services.map((service, index ) => {
+    return <Static key={index} service={service}/>}) :
+  services.length === 6 ?
+    <section className="wrapper">
+      <div className="buttons">
+        <button className="prevButton" onClick={handlePrevImage}>
+        <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>
+        </button>
+        <button className="nextButton "onClick={handleNextImage}>
+          <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1l99.9 107.1c3.5 3.8 5.5 8.7 5.5 13.8s-2 10.1-5.5 13.8L294.6 376.9c-4.2 4.5-10.1 7.1-16.3 7.1C266 384 256 374 256 361.7l0-57.7-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-57.7c0-12.3 10-22.3 22.3-22.3c6.2 0 12.1 2.6 16.3 7.1z"/></svg>
+        </button>
       </div>
-  {/* //
-  //
-  //
-
-  // return (<section className="container"> */}
-  {/* {services.length === 1 ? */}
-  {/* //     services.map((service, index ) => {
-  //       return <Static key={index} service={service}/>}) :
-  //   ''}
-
-  //   {services.length === 6 ?
-  //     <section className="wrapper">
-  //       <div className="buttons">
-  //         <button className="prevButton" onClick={handlePrevImage}>
-  //         <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>
-  //         </button>
-  //         <button className="nextButton "onClick={handleNextImage}>
-  //           <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1l99.9 107.1c3.5 3.8 5.5 8.7 5.5 13.8s-2 10.1-5.5 13.8L294.6 376.9c-4.2 4.5-10.1 7.1-16.3 7.1C266 384 256 374 256 361.7l0-57.7-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-57.7c0-12.3 10-22.3 22.3-22.3c6.2 0 12.1 2.6 16.3 7.1z"/></svg>
-  //         </button>
-  //       </div>
-  //       <Galery services={services} currentImage={currentImage}/>
-  //       <h1 className="name">{services[currentImage].name}</h1>
-  //       <h2 className="price">{services[currentImage].price}</h2>
-  //       <button className="add">añadir servicio</button>
-  //     </section> :
-  //   ''}
-  //   {services.length === 3 ?
-  //     <section className="wrapper">
-  //       <div className="buttons">
-  //         <button className="prevButton" onClick={handlePrevImage}>
-  //         <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>
-  //         </button>
-  //         <button className="nextButton "onClick={handleNextImage}>
-  //           <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1l99.9 107.1c3.5 3.8 5.5 8.7 5.5 13.8s-2 10.1-5.5 13.8L294.6 376.9c-4.2 4.5-10.1 7.1-16.3 7.1C266 384 256 374 256 361.7l0-57.7-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-57.7c0-12.3 10-22.3 22.3-22.3c6.2 0 12.1 2.6 16.3 7.1z"/></svg>
-  //         </button>
-  //       </div>
-  //       <Galery services={services} currentImage={currentImage}/>
-  //       <h1 className="name">{services[currentImage].name}</h1>
-  //       <h2 className="price">{services[currentImage].price}</h2>
-  //       <button className="add">añadir servicio</button>
-  //     </section> :
-  //   ''}
-
-  //   {services.length === 2 ?
-  //     <section className="wrapper">
-  //         <div className="buttons">
-  //           <button className="prevButton" onClick={handlePrevImage}>
-  //           <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>
-  //           </button>
-  //           <button className="nextButton "onClick={handleNextImage}>
-  //             <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1l99.9 107.1c3.5 3.8 5.5 8.7 5.5 13.8s-2 10.1-5.5 13.8L294.6 376.9c-4.2 4.5-10.1 7.1-16.3 7.1C266 384 256 374 256 361.7l0-57.7-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-57.7c0-12.3 10-22.3 22.3-22.3c6.2 0 12.1 2.6 16.3 7.1z"/></svg>
-  //           </button>
-  //       </div>
-  //       <Galery services={services} currentImage={currentImage}/>
-  //       <h1 className="name">{services[currentImage].name}</h1>
-  //       <h2 className="price">{services[currentImage].price}</h2>
-  //       <button className="add">añadir servicio</button>
-  //     </section> :
-    // ''} */}
+      <Galery services={services} currentImage={currentImage}/>
+      <h1 className="name">{services[currentImage].name}</h1>
+      <h2 className="price">{services[currentImage].price}</h2>
+      <button className="add">añadir servicio</button>
+    </section> :
+  services.length === 3 ?
+    <section className="wrapper">
+      <div className="buttons">
+        <button className="prevButton" onClick={handlePrevImage}>
+        <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>
+        </button>
+        <button className="nextButton "onClick={handleNextImage}>
+          <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1l99.9 107.1c3.5 3.8 5.5 8.7 5.5 13.8s-2 10.1-5.5 13.8L294.6 376.9c-4.2 4.5-10.1 7.1-16.3 7.1C266 384 256 374 256 361.7l0-57.7-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-57.7c0-12.3 10-22.3 22.3-22.3c6.2 0 12.1 2.6 16.3 7.1z"/></svg>
+        </button>
+      </div>
+      <Galery services={services} currentImage={currentImage}/>
+      <h1 className="name">{services[currentImage].name}</h1>
+      <h2 className="price">{services[currentImage].price}</h2>
+      <button className="add">añadir servicio</button>
+    </section> :
+  services.length === 2 ?
+    <section className="wrapper">
+        <div className="buttons">
+          <button className="prevButton" onClick={handlePrevImage}>
+          <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z"/></svg>
+          </button>
+          <button className="nextButton "onClick={handleNextImage}>
+            <svg viewBox="0 0 512 512"><path color="#3f0c3a" fill="currentcolor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1l99.9 107.1c3.5 3.8 5.5 8.7 5.5 13.8s-2 10.1-5.5 13.8L294.6 376.9c-4.2 4.5-10.1 7.1-16.3 7.1C266 384 256 374 256 361.7l0-57.7-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-57.7c0-12.3 10-22.3 22.3-22.3c6.2 0 12.1 2.6 16.3 7.1z"/></svg>
+          </button>
+      </div>
+      <Galery services={services} currentImage={currentImage}/>
+      <h1 className="name">{services[currentImage].name}</h1>
+      <h2 className="price">{services[currentImage].price}</h2>
+      <button className="add">añadir servicio</button>
+    </section> : ''}
 
     <style jsx>{`
       .container {
@@ -296,6 +304,6 @@ const DescriptionCard = ({cardSelected}) => {
       }
     `}</style>
   </section>
-};
+  )};
 
 export default DescriptionCard;
