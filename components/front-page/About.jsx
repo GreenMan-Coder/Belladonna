485bc4becfd2139464a30bdb47e40feef4083f64
unknown <juan_diego_toro@hotmail.com>
2023-10-21 21:22:52 -0500

    Version 1. Frontpage working and responsive desing started with min.1 width.1

--- components/front-page/About.jsx
+++ components/front-page/About.jsx
@@ -2,24 +2,27 @@ import Post from "./about/Post"
 import Footer from "../footer/Footer"
 
 const About = () => {
-
-    return <section className="container">
-      <Post/>
-      <Footer/>
-
-      <style jsx>{`
-          .container {
-            height: 100%;
-            width: 100%;
-            margin: 0;
-            box-sizing: border-box;
-            margin-top: 10%;
-            background-color: var(--tone-purple);
-            font-family: var(--font);
-            display: grid;
-            grid-template-rows: 1fr 0.25fr;
-          }
-      `}</style>
-    </section>
+  return <section className="container">
+    <Post/>
+    <Footer/>
+    <style jsx>{`
+      .container {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        box-sizing: border-box;
+        margin-top: 10%;
+        background-color: var(--tone-purple);
+        font-family: var(--font);
+        display: grid;
+        grid-template-rows: 1fr 0.25fr;
+      }
+      @media screen and (max-width: 280px) {
+    .container{
+      border: 1px solid red;
+    }
+    }
+    `}</style>
+  </section>
 }
 export default About
